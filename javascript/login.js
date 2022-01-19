dd0f4e5ab33b06ab6a562eec1149e215cc35a205
JeremyHTH <jeremy.hung56@gmail.com>
2022-01-19 14:14:45 +0800

    login page update

--- javascript/login.js
+++ javascript/login.js
@@ -1,10 +1,11 @@
 
-document.getElementsById("login button").addEventListener("click",Login);
+document.getElementById("login button").addEventListener("click",Login);
 
 function Login()
 {
-    inputname = document.getElementByName("username").innerHTML;
-    inputpassword = document.getElementByName("password").innerHTML;
-    alert(inputname + inputpassword)
+    // inputname = document.getElementByName("username").innerHTML;
+    // inputpassword = document.getElementByName("password").innerHTML;
+    alert("click")
     window.open("/HTML/Home.html", "_self")
+    
 }
